8c06177cd6ae2fca5f6e2ebf1a1e3e7f3eb618e9
efwoods <efwoods@g.clemson.edu>
2018-04-11 14:58:25 -0400

    build

--- dc_motor_encoder_hardware_simulated_slrt_rtw/rtGetInf.h
+++ dc_motor_encoder_hardware_simulated_slrt_rtw/rtGetInf.h
@@ -10,7 +10,7 @@
  *
  * Model version              : 1.100
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C source code generated on : Wed Apr 11 14:45:56 2018
+ * C source code generated on : Wed Apr 11 14:55:53 2018
  *
  * Target selection: slrt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
