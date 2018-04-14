3d836fc0c09f4700e31266081ddfaf3f995efc13
efwoods <efwoods@g.clemson.edu>
2018-04-14 10:14:23 -0400

    update

--- arduino_test.m
+++ arduino_test.m
@@ -3,17 +3,14 @@ clear,clc,close all;
 % build and start the motor control 
 simulation_name = 'dc_motor_encoder_hardware_simulated'
 rtwbuild(simulation_name)
-tg=xpc;
-load(tg,simulation_name)
-tg.start
-tg.status 
-%%
-[stepper, servo, a] = arduino_setup
 hw = struct %hardware
-hw.stepper= stepper
-hw.servo = servo
-hw.a = a
-hw.tg = tg
+hw.tg=xpc;
+load(hw.tg,simulation_name)
+hw.tg.start
+hw.tg.status 
+%%
+[hw.stepper, hw.servo, hw.a] = arduino_setup
+% 
 % disp('%d', tg.getsignal('Q4 AD'))
 pause(1)
 % test(hw)
